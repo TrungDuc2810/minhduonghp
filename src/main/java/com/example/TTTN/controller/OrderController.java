016e7498e4ad01035c83a3fb1198c75547786fc0
xuanquytb <vuxuanquynnn@gmail.com>
2025-04-29 15:32:26 +0700

    process get data analyze

--- src/main/java/com/example/TTTN/controller/OrderController.java
+++ src/main/java/com/example/TTTN/controller/OrderController.java
@@ -1,14 +1,15 @@
 package com.example.TTTN.controller;
 
 import com.example.TTTN.payload.ListResponse;
+import com.example.TTTN.payload.MonthlyRevenueDto;
 import com.example.TTTN.payload.OrderDto;
+import com.example.TTTN.payload.YearlyRevenueDto;
 import com.example.TTTN.service.OrderService;
 import com.example.TTTN.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 @RequestMapping("/api/orders")
@@ -19,7 +20,7 @@ public class OrderController {
         this.orderService = orderService;
     }
 
-//    @PreAuthorize("hasRole('ADMIN_KD')")
+    // @PreAuthorize("hasRole('ADMIN_KD')")
     @PostMapping
     public ResponseEntity<OrderDto> createOrder(@RequestBody OrderDto orderDto) {
         return new ResponseEntity<>(orderService.createOrder(orderDto), HttpStatus.CREATED);
@@ -30,8 +31,7 @@ public class OrderController {
             @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
             @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
             @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
-    ) {
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir) {
         return orderService.getAllOrders(pageNo, pageSize, sortBy, sortDir);
     }
 
@@ -41,8 +41,7 @@ public class OrderController {
             @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
             @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
             @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
-    ) {
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir) {
         return orderService.getOrdersByPartnerId(id, pageNo, pageSize, sortBy, sortDir);
     }
 
@@ -51,9 +50,10 @@ public class OrderController {
         return new ResponseEntity<>(orderService.getOrderById(id), HttpStatus.OK);
     }
 
-//    @PreAuthorize("hasRole('ADMIN_KD')")
+    // @PreAuthorize("hasRole('ADMIN_KD')")
     @PutMapping("/{id}")
-    public ResponseEntity<OrderDto> updateOrder(@PathVariable(name = "id") long orderId, @RequestBody OrderDto orderDto) {
+    public ResponseEntity<OrderDto> updateOrder(@PathVariable(name = "id") long orderId,
+            @RequestBody OrderDto orderDto) {
         return new ResponseEntity<>(orderService.updateOrder(orderId, orderDto), HttpStatus.OK);
     }
 
@@ -61,5 +61,20 @@ public class OrderController {
     public ResponseEntity<String> deleteOrderById(@PathVariable(name = "id") long id) {
         orderService.deleteOrderById(id);
         return new ResponseEntity<>("Delete successfully!!!", HttpStatus.OK);
+    }
+
+    @GetMapping("/monthly-revenue")
+    public ListResponse<MonthlyRevenueDto> getMonthlyRevenue(@RequestParam() int year) {
+        return orderService.getMonthlyRevenue(year);
+    }
+
+    @GetMapping("/quarterly-revenue")
+    public ListResponse<MonthlyRevenueDto> getQuarterlyRevenue(@RequestParam() int year) {
+        return orderService.getQuarterlyRevenue(year);
+    }
+
+    @GetMapping("/year-revenue")
+    public ListResponse<YearlyRevenueDto> getYearRevenue() {
+        return orderService.getYearRevenue();
     }
 }
--- src/main/java/com/example/TTTN/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/TTTN/service/impl/OrderServiceImpl.java
@@ -3,7 +3,9 @@ package com.example.TTTN.service.impl;
 import com.example.TTTN.entity.*;
 import com.example.TTTN.exception.ResourceNotFoundException;
 import com.example.TTTN.payload.ListResponse;
+import com.example.TTTN.payload.MonthlyRevenueDto;
 import com.example.TTTN.payload.OrderDto;
+import com.example.TTTN.payload.YearlyRevenueDto;
 import com.example.TTTN.repository.OrderRepository;
 import com.example.TTTN.repository.OrderStatusRepository;
 import com.example.TTTN.repository.OrderTypeRepository;
@@ -16,157 +18,248 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
-    private final OrderRepository orderRepository;
-    private final ModelMapper modelMapper;
-    private final PartnerRepository partnerRepository;
-    private final OrderStatusRepository orderStatusRepository;
-    private final OrderTypeRepository orderTypeRepository;
+        private final OrderRepository orderRepository;
+        private final ModelMapper modelMapper;
+        private final PartnerRepository partnerRepository;
+        private final OrderStatusRepository orderStatusRepository;
+        private final OrderTypeRepository orderTypeRepository;
 
-    public OrderServiceImpl(OrderRepository orderRepository, ModelMapper modelMapper, PartnerRepository partnerRepository, OrderStatusRepository orderStatusRepository, OrderTypeRepository orderTypeRepository) {
-        this.orderRepository = orderRepository;
-        this.modelMapper = modelMapper;
-        this.partnerRepository = partnerRepository;
-        this.orderStatusRepository = orderStatusRepository;
-        this.orderTypeRepository = orderTypeRepository;
-    }
-
-    private OrderDto mapToDto(Order order) {
-        return modelMapper.map(order, OrderDto.class);
-    }
-
-    private Order mapToEntity(OrderDto orderDto) {
-        return modelMapper.map(orderDto, Order.class);
-    }
-
-    @Override
-    @Transactional
-    public OrderDto createOrder(OrderDto orderDto) {
-        OrderStatus orderStatus = orderStatusRepository.findByName("Chưa thanh toán");
-        orderDto.setOrderStatusId(orderStatus.getId());
-        orderDto.setPaidMoney(0);
-
-        Partner partner = partnerRepository.findById(orderDto.getPartnerId()).orElseThrow(()
-                -> new ResourceNotFoundException("Partner", "id", String.valueOf(orderDto.getPartnerId())));
-        partner.setDebt(partner.getDebt() + orderDto.getTotalMoney());
-        partnerRepository.save(partner);
-
-        Order order = mapToEntity(orderDto);
-        return mapToDto(orderRepository.save(order));
-    }
-
-    @Override
-    public ListResponse<OrderDto> getAllOrders(int pageNo, int pageSize, String sortBy, String sortDir) {
-        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())
-                ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
-
-        PageRequest pageRequest = PageRequest.of(pageNo, pageSize, sort);
-
-        Page<Order> orders = orderRepository.findAll(pageRequest);
-
-        List<Order> listOfOrders = orders.getContent();
-
-        List<OrderDto> content = listOfOrders.stream().map(this::mapToDto).toList();
-
-        ListResponse<OrderDto> orderResponse = new ListResponse<>();
-        orderResponse.setContent(content);
-        orderResponse.setPageNo(orders.getNumber());
-        orderResponse.setPageSize(orders.getSize());
-        orderResponse.setTotalPages(orders.getTotalPages());
-        orderResponse.setTotalElements((int)orders.getTotalElements());
-        orderResponse.setLast(orders.isLast());
-
-        return orderResponse;
-
-    }
-
-    @Override
-    public ListResponse<OrderDto> getOrdersByPartnerId(long id, int pageNo, int pageSize, String sortBy, String sortDir) {
-        partnerRepository.findById(id).orElseThrow(()
-                -> new ResourceNotFoundException("Order", "id", String.valueOf(id)));
-
-        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())
-                ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
-
-        PageRequest pageRequest = PageRequest.of(pageNo, pageSize, sort);
-
-        Page<Order> orders = orderRepository.findByPartnerId(id, pageRequest);
-
-        List<Order> listOfOrders = orders.getContent();
-
-        List<OrderDto> content = listOfOrders.stream().map(this::mapToDto).toList();
-
-        ListResponse<OrderDto> orderResponse = new ListResponse<>();
-        orderResponse.setContent(content);
-        orderResponse.setPageNo(orders.getNumber());
-        orderResponse.setPageSize(orders.getSize());
-        orderResponse.setTotalPages(orders.getTotalPages());
-        orderResponse.setTotalElements((int)orders.getTotalElements());
-        orderResponse.setLast(orders.isLast());
-
-        return orderResponse;
-    }
-
-    @Override
-    public OrderDto getOrderById(long orderId) {
-        Order order = orderRepository.findById(orderId).orElseThrow(()
-                -> new ResourceNotFoundException("Order", "id", String.valueOf(orderId)));
-        return mapToDto(order);
-    }
-
-    @Override
-    @Transactional
-    public OrderDto updateOrder(long orderId, OrderDto orderDto) {
-        if (orderDto.getTotalMoney() < orderDto.getPaidMoney()) {
-            throw new IllegalArgumentException("Tổng giá trị đơn hàng không thể nhỏ hơn tổng tiền đã trả.");
+        public OrderServiceImpl(OrderRepository orderRepository, ModelMapper modelMapper,
+                        PartnerRepository partnerRepository, OrderStatusRepository orderStatusRepository,
+                        OrderTypeRepository orderTypeRepository) {
+                this.orderRepository = orderRepository;
+                this.modelMapper = modelMapper;
+                this.partnerRepository = partnerRepository;
+                this.orderStatusRepository = orderStatusRepository;
+                this.orderTypeRepository = orderTypeRepository;
         }
 
-        Order order = orderRepository.findById(orderId)
-                .orElseThrow(() -> new ResourceNotFoundException("Order", "id", String.valueOf(orderId)));
-
-        Partner partner = partnerRepository.findById(orderDto.getPartnerId())
-                .orElseThrow(() -> new ResourceNotFoundException("Partner", "id", String.valueOf(orderDto.getPartnerId())));
-
-        OrderType orderType = orderTypeRepository.findById(orderDto.getOrderTypeId())
-                .orElseThrow(() -> new ResourceNotFoundException("Order type", "id", String.valueOf(orderDto.getOrderTypeId())));
-
-        String statusName = orderDto.getPaidMoney() == 0.0 ? "Chưa thanh toán" :
-                orderDto.getPaidMoney() < orderDto.getTotalMoney() ? "Thanh toán một phần" :
-                        "Đã thanh toán";
-
-        OrderStatus orderStatus = orderStatusRepository.findByName(statusName);
-        if (orderStatus == null) {
-            throw new ResourceNotFoundException("Order status", "name", statusName);
+        private OrderDto mapToDto(Order order) {
+                return modelMapper.map(order, OrderDto.class);
         }
 
-        double oldDebt = order.getTotalMoney() - order.getPaidMoney();
-        double newDebt = orderDto.getTotalMoney() - orderDto.getPaidMoney();
-        partner.setDebt(partner.getDebt() - oldDebt + newDebt);
-        partnerRepository.save(partner);
+        private Order mapToEntity(OrderDto orderDto) {
+                return modelMapper.map(orderDto, Order.class);
+        }
 
-        order.setTotalMoney(orderDto.getTotalMoney());
-        order.setPaidMoney(orderDto.getPaidMoney());
-        order.setOrderStatus(orderStatus);
-        order.setOrderType(orderType);
-        order.setPartner(partner);
+        @Override
+        @Transactional
+        public OrderDto createOrder(OrderDto orderDto) {
+                OrderStatus orderStatus = orderStatusRepository.findByName("Chưa thanh toán");
+                orderDto.setOrderStatusId(orderStatus.getId());
+                orderDto.setPaidMoney(0);
 
-        orderRepository.save(order);
-        return mapToDto(order);
-    }
+                Partner partner = partnerRepository.findById(orderDto.getPartnerId()).orElseThrow(
+                                () -> new ResourceNotFoundException("Partner", "id",
+                                                String.valueOf(orderDto.getPartnerId())));
+                partner.setDebt(partner.getDebt() + orderDto.getTotalMoney());
+                partnerRepository.save(partner);
 
+                Order order = mapToEntity(orderDto);
+                return mapToDto(orderRepository.save(order));
+        }
 
-    @Override
-    @Transactional
-    public void deleteOrderById(long orderId) {
-        Order order = orderRepository.findById(orderId)
-                .orElseThrow(() -> new ResourceNotFoundException("Order", "id", String.valueOf(orderId)));
+        @Override
+        public ListResponse<OrderDto> getAllOrders(int pageNo, int pageSize, String sortBy, String sortDir) {
+                Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())
+                                ? Sort.by(sortBy).ascending()
+                                : Sort.by(sortBy).descending();
 
-        Partner partner = order.getPartner();
-        partner.setDebt(partner.getDebt() + order.getPaidMoney() - order.getTotalMoney());
+                PageRequest pageRequest = PageRequest.of(pageNo, pageSize, sort);
 
-        orderRepository.delete(order);
-    }
+                Page<Order> orders = orderRepository.findAll(pageRequest);
+
+                List<Order> listOfOrders = orders.getContent();
+
+                List<OrderDto> content = listOfOrders.stream().map(this::mapToDto).toList();
+
+                ListResponse<OrderDto> orderResponse = new ListResponse<>();
+                orderResponse.setContent(content);
+                orderResponse.setPageNo(orders.getNumber());
+                orderResponse.setPageSize(orders.getSize());
+                orderResponse.setTotalPages(orders.getTotalPages());
+                orderResponse.setTotalElements((int) orders.getTotalElements());
+                orderResponse.setLast(orders.isLast());
+
+                return orderResponse;
+
+        }
+
+        @Override
+        public ListResponse<OrderDto> getOrdersByPartnerId(long id, int pageNo, int pageSize, String sortBy,
+                        String sortDir) {
+                partnerRepository.findById(id)
+                                .orElseThrow(() -> new ResourceNotFoundException("Order", "id", String.valueOf(id)));
+
+                Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())
+                                ? Sort.by(sortBy).ascending()
+                                : Sort.by(sortBy).descending();
+
+                PageRequest pageRequest = PageRequest.of(pageNo, pageSize, sort);
+
+                Page<Order> orders = orderRepository.findByPartnerId(id, pageRequest);
+
+                List<Order> listOfOrders = orders.getContent();
+
+                List<OrderDto> content = listOfOrders.stream().map(this::mapToDto).toList();
+
+                ListResponse<OrderDto> orderResponse = new ListResponse<>();
+                orderResponse.setContent(content);
+                orderResponse.setPageNo(orders.getNumber());
+                orderResponse.setPageSize(orders.getSize());
+                orderResponse.setTotalPages(orders.getTotalPages());
+                orderResponse.setTotalElements((int) orders.getTotalElements());
+                orderResponse.setLast(orders.isLast());
+
+                return orderResponse;
+        }
+
+        @Override
+        public OrderDto getOrderById(long orderId) {
+                Order order = orderRepository.findById(orderId)
+                                .orElseThrow(() -> new ResourceNotFoundException("Order", "id",
+                                                String.valueOf(orderId)));
+                return mapToDto(order);
+        }
+
+        @Override
+        @Transactional
+        public OrderDto updateOrder(long orderId, OrderDto orderDto) {
+                if (orderDto.getTotalMoney() < orderDto.getPaidMoney()) {
+                        throw new IllegalArgumentException("Tổng giá trị đơn hàng không thể nhỏ hơn tổng tiền đã trả.");
+                }
+
+                Order order = orderRepository.findById(orderId)
+                                .orElseThrow(() -> new ResourceNotFoundException("Order", "id",
+                                                String.valueOf(orderId)));
+
+                Partner partner = partnerRepository.findById(orderDto.getPartnerId())
+                                .orElseThrow(
+                                                () -> new ResourceNotFoundException("Partner", "id",
+                                                                String.valueOf(orderDto.getPartnerId())));
+
+                OrderType orderType = orderTypeRepository.findById(orderDto.getOrderTypeId())
+                                .orElseThrow(() -> new ResourceNotFoundException("Order type", "id",
+                                                String.valueOf(orderDto.getOrderTypeId())));
+
+                String statusName = orderDto.getPaidMoney() == 0.0 ? "Chưa thanh toán"
+                                : orderDto.getPaidMoney() < orderDto.getTotalMoney() ? "Thanh toán một phần"
+                                                : "Đã thanh toán";
+
+                OrderStatus orderStatus = orderStatusRepository.findByName(statusName);
+                if (orderStatus == null) {
+                        throw new ResourceNotFoundException("Order status", "name", statusName);
+                }
+
+                double oldDebt = order.getTotalMoney() - order.getPaidMoney();
+                double newDebt = orderDto.getTotalMoney() - orderDto.getPaidMoney();
+                partner.setDebt(partner.getDebt() - oldDebt + newDebt);
+                partnerRepository.save(partner);
+
+                order.setTotalMoney(orderDto.getTotalMoney());
+                order.setPaidMoney(orderDto.getPaidMoney());
+                order.setOrderStatus(orderStatus);
+                order.setOrderType(orderType);
+                order.setPartner(partner);
+
+                orderRepository.save(order);
+                return mapToDto(order);
+        }
+
+        @Override
+        @Transactional
+        public void deleteOrderById(long orderId) {
+                Order order = orderRepository.findById(orderId)
+                                .orElseThrow(() -> new ResourceNotFoundException("Order", "id",
+                                                String.valueOf(orderId)));
+
+                Partner partner = order.getPartner();
+                partner.setDebt(partner.getDebt() + order.getPaidMoney() - order.getTotalMoney());
+
+                orderRepository.delete(order);
+        }
+
+        @Override
+        public ListResponse<MonthlyRevenueDto> getMonthlyRevenue(int year) {
+                // Lấy dữ liệu từ repository
+                List<Object[]> revenueData = orderRepository.findRevenueByMonth(year);
+
+                // Chuyển đổi sang MonthlyRevenueDto
+                List<MonthlyRevenueDto> revenueList = revenueData.stream()
+                                .map(data -> {
+                                        int resultYear = ((Number) data[0]).intValue();
+                                        int quarter = ((Number) data[1]).intValue();
+                                        BigDecimal revenue = new BigDecimal(data[2].toString());
+                                        return new MonthlyRevenueDto(resultYear, quarter, revenue); // truyền BigDecimal
+                                })
+                                .toList();
+
+                // Tạo response
+                ListResponse<MonthlyRevenueDto> response = new ListResponse<>();
+                response.setContent(revenueList);
+                response.setPageNo(0);
+                response.setPageSize(revenueList.size());
+                response.setTotalPages(1);
+                response.setTotalElements(revenueList.size());
+                response.setLast(true);
+
+                return response;
+        }
+
+        @Override
+        public ListResponse<MonthlyRevenueDto> getQuarterlyRevenue(int year) {
+                // Lấy dữ liệu từ repository
+                List<Object[]> revenueData = orderRepository.findRevenueByQuarter(year);
+
+                // Chuyển đổi sang DTO
+                List<MonthlyRevenueDto> revenueList = revenueData.stream()
+                                .map(data -> {
+                                        int resultYear = ((Number) data[0]).intValue();
+                                        int quarter = ((Number) data[1]).intValue();
+                                        BigDecimal revenue = new BigDecimal(data[2].toString());
+                                        return new MonthlyRevenueDto(resultYear, quarter, revenue); // truyền BigDecimal
+                                })
+                                .toList();
+
+                // Tạo response
+                ListResponse<MonthlyRevenueDto> response = new ListResponse<>();
+                response.setContent(revenueList);
+                response.setPageNo(0);
+                response.setPageSize(revenueList.size());
+                response.setTotalPages(1);
+                response.setTotalElements(revenueList.size());
+                response.setLast(true);
+
+                return response;
+        }
+
+        @Override
+        public ListResponse<YearlyRevenueDto> getYearRevenue() {
+                List<Object[]> revenueData = orderRepository.findRevenueByYear();
+
+                List<YearlyRevenueDto> revenueList = revenueData.stream()
+                                .map(data -> {
+                                        int year = ((Number) data[0]).intValue();
+                                        BigDecimal revenue = new BigDecimal(data[1].toString());
+                                        return new YearlyRevenueDto(year, revenue.toPlainString());
+                                })
+                                .toList();
+
+                ListResponse<YearlyRevenueDto> response = new ListResponse<>();
+                response.setContent(revenueList);
+                response.setPageNo(0);
+                response.setPageSize(revenueList.size());
+                response.setTotalPages(1);
+                response.setTotalElements(revenueList.size());
+                response.setLast(true);
+
+                return response;
+        }
+
 }
