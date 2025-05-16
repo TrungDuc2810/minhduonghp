e3572e53bea696f6ce9397deae8a49d8c34138d3
Nguyen Trung Duc <ntduc2810@gmail.com>
2025-05-16 17:20:01 +0700

    update logic for warehouse transaction

--- src/main/java/com/example/TTTN/payload/ImportBatchDto.java
+++ src/main/java/com/example/TTTN/payload/ImportBatchDto.java
@@ -6,9 +6,12 @@ import java.time.LocalDateTime;
 @Data
 public class ImportBatchDto {
     private long id;
-    private int quantityRemaining;
+    private int remainQuantity;
+    private int importQuantity;
     private double unitCost;
     private LocalDateTime importDate;
+    private LocalDateTime expireDate;
     private long productId;
     private long warehouseId;
+    private long warehouseTransactionId;
 }
